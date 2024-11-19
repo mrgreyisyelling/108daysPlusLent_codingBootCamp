32973f5f74ee346863c0bb5b941a1bbbb6945778
Michael Penta <mike@thefivepointsoflansing.com>
2024-11-19 16:21:38 +0700

    Set up gasleft() code

--- Last 10 days in Bangkok/Conversations_with_ASH/Day_2/projects/contracts/GasTracker.sol
+++ Last 10 days in Bangkok/Conversations_with_ASH/Day_2/projects/contracts/GasTracker.sol
@@ -1,22 +1,38 @@
-//spsometihng
+// SPDX-License-Identifier: MIT
 pragma solidity ^0.8.27;
 
 
 contract GasTracker{
+    event GasUsed(string functionName, uint256 GasUsed);
 
     function gasHeavy(uint256 seedNumber) public {
-        uint256 counter = 0
+        unit256 gasStart = gasleft();
+
+        uint256 counter = 0;
         for (uint i = 0; i < seedNumber; i++) {
             counter += i+i+i;
         }
+
+        unit256 gasEnd = gasleft();
+        emit GasUsed("gasHeavy", gasStart-gasEnd);
     }
 
     function gasLight(uint256 seednumber) public{
+        unit256 gasStart = gasleft();
+
         seedNumber+seedNumber;
+
+        unit256 gasEnd = gasleft();
+        emit GasUsed("gasHeavy", gasStart-gasEnd);
     }
 
     function gasIntermediate(uint256 seednumber) public{
-        seedNumber*seedNumber
+        unit256 gasStart = gasleft();
+        
+        seedNumber*seedNumber;
+
+        unit256 gasEnd = gasleft();
+        emit GasUsed("gasHeavy", gasStart-gasEnd);
     }
 
 
